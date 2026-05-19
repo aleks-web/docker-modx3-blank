de33c12712db4ce63cc20fc579141873ddaf0ded
Alex <dok.go@yandex.ru>
2026-05-19 15:18:52 +0500

    fc

---
+++ scripts/modx/cast/cast/components/tinymcerte/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'modxcms/tinymcerte',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'fa05cd8ddabd93df4ef57800e0b21da10c859e39',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'modxcms/tinymcerte' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fa05cd8ddabd93df4ef57800e0b21da10c859e39',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
